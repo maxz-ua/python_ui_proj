a518ea6035798e0d9a30aa1edd00bc08d99e2e9b
maxz.ua <maxz.ua@gmail.com>
2024-02-09 18:26:51 +0200

    Added some POM structure

---
+++ generator/generator.py
@@ -0,0 +1,14 @@
+from data.data import Person
+from faker import Faker
+
+faker = Faker('en_GB')
+Faker.seed()
+
+
+def generated_person():
+    yield Person(
+        full_name=faker.first_name() + " " + faker.last_name(),
+        email=faker.email(),
+        current_address=faker.address(),
+        permanent_address=faker.address(),
+    )
---
+++ tests/elements_test.py
@@ -0,0 +1,15 @@
+import time
+
+from pages.elements_page import TextBoxPage
+
+
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+            input_data = text_box_page.fill_all_fields()
+            output_data = text_box_page.check_filled_form()
+            time.sleep(5)
+            assert input_data == output_data
