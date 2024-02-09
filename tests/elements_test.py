3a2ac34b2ed8cbe196fe68b794e564d105db6721
maxz.ua <maxz.ua@gmail.com>
2024-02-09 19:55:40 +0200

    Added boxPage

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 class TestElements:
@@ -11,5 +11,15 @@ class TestElements:
             text_box_page.open()
             input_data = text_box_page.fill_all_fields()
             output_data = text_box_page.check_filled_form()
-            time.sleep(5)
             assert input_data == output_data
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkboxes = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            time.sleep(3)
+            assert input_checkboxes == output_result, 'Test assertion: no checkboxes'
